02d4e3d98d8fc325efa8cbaa934e414d50e5d147
chanan helman <chananhelman@gmail.com>
2020-07-29 10:21:06 +0300

    count button

--- TK.py
+++ TK.py
@@ -1,9 +1,10 @@
 from tkinter import *
 
 count = 0
-def counter_label():
-   global count
-   count +=1
+def counter_label(label):
+    global count
+    count +=1
+    return count
 
 window = Tk()
 
@@ -14,8 +15,9 @@ label = Label(window, text = "Hello everyone", fg = "dark green").pack()
 
 #here I want to show the counter
 
-label1 = Label(window, text=count).pack()
+label1 = Label(window, text= "count = ").pack()
+label2 = Label(window, text = count).pack()
 
-button = Button(window, text = "push",command = counter_label, fg = "blue", width = 10, height = 3, bg = "#fff").pack()
+button = Button(window, text = "push",command = counter_label(label2), fg = "blue", width = 10, height = 3, bg = "#fff").pack()
 
 window.mainloop()
